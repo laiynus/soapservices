cc951bd2bf541b25f269096bfd53788cd3a43d4f
Raman Khrapavitski <Raman_Khrapavitski@epam.com>
2016-06-16 12:01:58 +0300

    gradle task sortProperties done

--- src/main/java/com/epam/khrapavitski/soapservice/task/Sheduler.java
+++ src/main/java/com/epam/khrapavitski/soapservice/task/Sheduler.java
@@ -16,12 +16,12 @@ public class Sheduler {
     @Qualifier("saveCurrency")
     Task saveCurrency;
 
-    @Scheduled(cron = "0 0 10 * * ?")
+    @Scheduled(cron = "0 0 12 * * ?")
     public void saveWeather() {
         saveWeather.execute();
     }
 
-    @Scheduled(cron = "0 0 10 * * ?")
+    @Scheduled(cron = "0 0 12 * * ?")
     public void saveCurrency() {
         saveCurrency.execute();
     }
