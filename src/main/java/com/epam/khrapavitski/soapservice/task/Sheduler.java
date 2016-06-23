28bf2f79dcda82e551427b5c1a4a8a0e71a1e36a
Raman Khrapavitski <Raman_Khrapavitski@epam.com>
2016-06-23 14:20:42 +0300

    added two liquibase profiles: test and production

--- src/main/java/com/epam/khrapavitski/soapservice/task/Sheduler.java
+++ src/main/java/com/epam/khrapavitski/soapservice/task/Sheduler.java
@@ -16,12 +16,12 @@ public class Sheduler {
     @Qualifier("saveCurrency")
     Task saveCurrency;
 
-    @Scheduled(cron = "0 0 12 * * ?")
+    @Scheduled(cron = "0 00 12 * * ?")
     public void saveWeather() {
         saveWeather.execute();
     }
 
-    @Scheduled(cron = "0 0 12 * * ?")
+    @Scheduled(cron = "0 00 12 * * ?")
     public void saveCurrency() {
         saveCurrency.execute();
     }
